246a5a9bf1d7eafe6b4cbd859b72feb7701d9d59
Administrator <Administrator@PC-20181103QGZB>
2018-11-13 23:36:06 +0800

    test2

--- src/main/java/ren/springboot/controller/UserController.java
+++ src/main/java/ren/springboot/controller/UserController.java
@@ -4,13 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Event;
+import com.dianping.cat.message.Transaction;
+
 @Controller
 public class UserController {
 	
 	@RequestMapping("hello.do")
 	@ResponseBody
 	public String hello(){
+		String pageName = "renyuanhang";
+		String serverIp = "2222";
+		double amount = 0;
+		Transaction t = Cat.newTransaction("URL", pageName);
 		
+		Cat.logEvent("URL.server", serverIp, Event.SUCCESS, "ip:111");
+		
+		Cat.logMetricForCount("Pay");
+		
+		Cat.logMetricForSum("Paya", amount);
+		
+		t.setStatus(Transaction.SUCCESS);
+		
+		t.complete();
+		System.out.println("hello");
 		return  "hello world!";
 	}
 
